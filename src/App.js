e2b988bc2ddd8ab326ea64d9e67ad0ebadd78a30
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-04-28 23:35:27 +0200

    zmiana implementacji

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import {Navbar, Sidebar, Footer } from './components'
 
+
+//import About from './pages/AboutPage'
 //import styled from 'styled-components'
 
 
