f42aa5aca065e63d5c0f2493a46237cfdfe354ab
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-03 23:53:55 +0200

    home page repair

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,12 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return (
+  <Wrapper className='section-center'>
+    <article className='content'></article>
+    
+  </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
