6c34eb0ac5f9e1c81bcb3828d9b572bd16e3354e
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-04 23:56:42 +0200

    contact aktualizacja

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+  <Wrapper>
+    <div className='section-center'>
+      <h3>Join our newsletter and get 20% off</h3>
+      <div className='content'>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Maecenas at vestibulum arcu. 
+        Nunc nunc ipsum, facilisis sed risus eu, ultricies.
+        </p>
+        <form className='contact-form'>
+          <input type='email'
+          className='form-input'
+          placeholder='enter email'
+          />
+          <button type='submit' className='submit-btn'>
+            subscribe
+          </button>
+        </form>
+      </div>
+    </div>
+  </Wrapper>
+  )
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
