ca40bebfda491eb8eb19e6019f70f4d67d793f4f
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-12 22:54:45 +0200

    sort price, a-z, z-a

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -10,8 +10,27 @@ const Sort = () => {
   return (
   <Wrapper>
     <div className='btn-container'>
+      <button type='button' className={`${grid_view?'active' : null}`}>
+        <BsFillGridFill/>
+      </button>
 
+      <button type='button' className={`${!grid_view?'active' : null}`}>
+        <BsList/>
+      </button>
     </div>
+    <p>
+      {products.length} products found
+    </p>
+    <hr/>
+    <form>
+      <label htmlFor='sort'>sort by</label>
+      <select name='sort' id='sort' className='sort-input'>
+        <option value='price-lowest'>price (lowest)</option>
+        <option value='price-highest'>price (highest)</option>
+        <option value='name-a'>name (a-z)</option>
+        <option value='name-z'>name (z-a)</option>
+      </select>
+    </form>
   </Wrapper>
 )
 }
