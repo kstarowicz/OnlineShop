a66ad9b26d927dde800278900ad37ef3d106d3dc
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-13 12:30:16 +0200

    controled add

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -8,7 +8,6 @@ const ProductList = () => {
   useFilterContext()
   if(products.length < 1){
     return (
-
     <h5 style= {{textTransform: 'none' }}>     
         Sorry, no products matched your search...
     </h5>
--- src/components/Sort.js
+++ src/components/Sort.js
@@ -6,7 +6,14 @@ import styled from 'styled-components'
 
 
 const Sort = () => {
-  const {filtered_products: products,grid_view,setGridView,setListView} =useFilterContext()
+  const {
+    filtered_products: products,
+    grid_view,
+    setGridView,
+    setListView,
+    sort,
+    updateSort,
+  } =useFilterContext()
   return (
   <Wrapper>
     <div className='btn-container'>
@@ -32,7 +39,13 @@ const Sort = () => {
     <hr/>
     <form>
       <label htmlFor='sort'>sort by</label>
-      <select name='sort' id='sort' className='sort-input'>
+      <select 
+      name='sort' 
+      id='sort' 
+      className='sort-input'
+      value={sort}
+       onChange={updateSort}
+      >
         <option value='price-lowest'>price (lowest)</option>
         <option value='price-highest'>price (highest)</option>
         <option value='name-a'>name (a-z)</option>
