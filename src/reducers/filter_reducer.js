d65744fa716c5b51c12597ea8cbd8038a2719ab4
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-13 12:48:40 +0200

    input controler sort

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -23,7 +23,9 @@ import {
     if(action.type === SET_LISTVIEW) {
       return {...state,grid_view:false}
     }
-    
+    if(action.type === UPDATE_SORT) {
+      return{...state, sort: action.payload }
+    }
     
     throw new Error(`No Matching "${action.type}" - action type`)
   }
