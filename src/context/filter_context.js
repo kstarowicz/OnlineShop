bb04adf3c25e38817448dc0742ab0b9ff903d661
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-14 17:19:29 +0200

    filters price add

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -72,11 +72,13 @@ export const FilterProvider = ({ children }) => {
     if (name === 'color'){
       value = e.target.dataset.color
     }
+    if(name === 'price'){
+      value = Number(value)
+    }
     dispatch({type: UPDATE_FILTERS, payload: { name, value } })
   }
 
-  const clearFilters = () => {
-  }
+  const clearFilters = () => {}
   return (
     <FilterContext.Provider 
     value={{
