3ebefc5f52ef4134565d0dc340d8005bcbd1067b
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-15 21:22:58 +0200

    update cart reducer

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -5,12 +5,27 @@ import {
     REMOVE_CART_ITEM,
     TOGGLE_CART_ITEM_AMOUNT,
   } from '../actions'
+import CartItem from '../components/CartItem';
   
   const cart_reducer = (state, action) => {
     if(action.type === ADD_TO_CART) {
-      const {id,color,amount,product} = action.payload;
+      const {id, color, amount,  product} = action.payload;
       const tempItem = state.cart.find((i) => i.id === id + color)
       if(tempItem) {
+        const tempCart = state.cart.map((cartItem) =>{
+          if(cartItem.id === id + color){
+            let newAmount = cartItem.amount = amount;
+            if(newAmount > cartItem.max){
+              newAmount = cartItem.max
+            }
+            return {...cartItem,amount: newAmount}
+          }
+          else{
+            return cartItem
+          }
+        })
+
+        return {...state,cart:tempCart}
 
       }else{
         const newItem = {
@@ -22,7 +37,6 @@ import {
           price: product.price,
           max: product.stock,
         }
-
           return {...state,cart:[...state.cart,newItem]}
         
       }
