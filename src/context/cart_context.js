53bafb909a84e2608e12d63de81d3be0b8bcf345
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-16 17:30:22 +0200

    add localStorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,11 +8,23 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
+
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart');
+  if(cart){
+    return JSON.parse(localStorage.getItem('cart'))
+
+  }
+  else{
+    return []
+  }
+}
+
 const initialState = {
-  cart: [],
-  total_items:0,
-  total_amount:0,
-  shipping_fee:534,
+  cart: getLocalStorage(),
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 534,
 }
 
 const CartContext = React.createContext()
@@ -33,6 +45,10 @@ export const CartProvider = ({ children }) => {
 
   const clearCart = ()=> {}
 
+  useEffect(() => {
+
+    localStorage.setItem('cart',JSON.stringify(state.cart))
+  }, [state.cart])
   return (
     <CartContext.Provider value={{...state, addToCart, removeItem, toggleAmount, clearCart}}>
       {children}
