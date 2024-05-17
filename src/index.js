69d851d6b19571df35b3c37cefd406247d61d9c4
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-17 18:31:01 +0200

    authO add

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';//spr czy działa bez client możliwy błąd bez '/client'
 import './index.css';
 import App from './App';
-
 import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
 import { CartProvider } from './context/cart_context';
@@ -11,8 +10,20 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 
 const container = document.getElementById('root'); // Pobierz element DOM
+//dev-lbn8zxj2ypy0m2ji.us.auth0.com
+//nUzdFBvSxE9dbjUhqWly4BWhXZk887NF
+
+
+
 const root = createRoot(container); // Utwórz root przy użyciu createRoot nowy sposób od 18 
 root.render(
+  <Auth0Provider
+    domain="dev-lbn8zxj2ypy0m2ji.us.auth0.com"
+    clientId="nAQ2v28A4ikg4ihXPbdl9pcNIdS6sd39"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
   <ProductsProvider>
     <FilterProvider>
       <CartProvider>
@@ -21,6 +32,7 @@ root.render(
     </FilterProvider>
     
   </ProductsProvider>
+  </Auth0Provider>
 );
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
