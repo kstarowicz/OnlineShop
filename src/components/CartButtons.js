aa2ae5eefafbe13394c46668ea606fe160c75bf1
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-25 12:51:12 +0200

    change cart button

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -23,16 +23,16 @@ const CartButtons = () => {
           
       </span>
     </Link>
-    <button type='button' className='auth-btn' onClick={loginWithRedirect}>
-      Login <FaUserPlus/>
-    </button>
-    <button 
+    {myUser ? <button 
     type='button' 
     className='auth-btn' 
-    onClick={() => logout({returnTo:window.location.origin})}
+    onClick={() => logout({ returnTo: window.location.origin })}
     >
       Logout <FaUserMinus/>
-    </button>
+    </button> : <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+      Login <FaUserPlus/>
+    </button>}
+    
    </Wrapper>
   )
 }
