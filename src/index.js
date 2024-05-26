03e0b2ee394a8a02f8db57251dad3207cf8579a9
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-26 20:06:59 +0200

    improvement fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';//spr czy działa bez client możliwy błąd bez '/client'
+import ReactDOM from 'react-dom/client';
+//import { createRoot } from 'react-dom/client';//spr czy działa bez client możliwy błąd bez '/client'
 import './index.css';
 import App from './App';
 import { ProductsProvider } from './context/products_context';
@@ -9,20 +10,19 @@ import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 
-const container = document.getElementById('root'); // Pobierz element DOM
+//const container = document.getElementById('root'); // Pobierz element DOM
 //dev-lbn8zxj2ypy0m2ji.us.auth0.com
 //nUzdFBvSxE9dbjUhqWly4BWhXZk887NF
 
 
 
-const root = createRoot(container); // Utwórz root przy użyciu createRoot  od 18 
+const root = ReactDOM.createRoot(document.getElementById('root')); // Utwórz root przy użyciu createRoot  od 18 
 root.render(
   <Auth0Provider
-    domain="dev-lbn8zxj2ypy0m2ji.us.auth0.com"
-    clientId="nAQ2v28A4ikg4ihXPbdl9pcNIdS6sd39"
-    authorizationParams={{
-   redirect_uri: window.location.origin
-    }}
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    redirectUri={window.location.origin}
+    cacheLocation = 'localstorage'
     >
     <UserProvider>
   <ProductsProvider>
@@ -39,3 +39,10 @@ root.render(
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 //root.render(<App />);
+
+
+// {/* <Auth0Provider
+// domain="dev-lbn8zxj2ypy0m2ji.us.auth0.com"
+// clientId="nAQ2v28A4ikg4ihXPbdl9pcNIdS6sd39"
+// authorizationParams={{
+// redirect_uri: window.location.origin */}
