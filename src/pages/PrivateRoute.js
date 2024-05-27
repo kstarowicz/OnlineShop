877ea687ea4255bc4e448a8965c5d6b7a145f51d
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-05-28 01:39:17 +0200

    featured + product Images

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 // will remove later
-import { useUserContext } from '../context/user_context';
+//import { useUserContext } from '../context/user_context';
 import { useAuth0 } from '@auth0/auth0-react';
 
 
 const PrivateRoute = ({children,...rest}) => {
   const {user} = useAuth0();
   return ( 
-  <Route {...rest} render={()=>{
+  <Route {...rest} 
+  render={()=>{
     return user ? children : <Redirect to='/'>
-      
     </Redirect>
   }}></Route>
 )};
