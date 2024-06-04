1319b1f805fe43f071ef23608637d67505cb11a9
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-06-04 13:34:22 +0200

    striap chekout

--- functions/hello.js
+++ functions/hello.js
@@ -3,9 +3,9 @@ const items = [
     {id:1, name: 'karolina'},
     {id:2, name: 'marek'},
 ]
-exports.handler = async function(event,context){
+exports.handler = async function (event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify(items),
+        body: 'hello world',
     }
 }
--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -10,7 +10,7 @@ import { useUserContext } from '../context/user_context'
 const CartButtons = () => {
 
   const { closeSidebar} = useProductsContext()
-  const {total_items} = useCartContext()
+  const {total_items, clearCart } = useCartContext()
   const {loginWithRedirect, myUser, logout} = useUserContext();
 
   return (
@@ -26,10 +26,15 @@ const CartButtons = () => {
     {myUser ? <button 
     type='button' 
     className='auth-btn' 
-    onClick={() => logout({ returnTo: window.location.origin })}
+    onClick={() =>{
+      clearCart()
+      logout({ returnTo: window.location.origin })
+    } 
+    }
     >
       Logout <FaUserMinus/>
     </button> : <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+
       Login <FaUserPlus/>
     </button>}
     
